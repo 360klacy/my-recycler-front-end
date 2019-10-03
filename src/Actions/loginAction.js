f19389478f06240822083f68c90b28d95a37b7cc
Kyle Lacy <kylelacy@Kyles-MacBook-Air.local>
2019-10-03 14:57:28 -0400

    Created categories files

--- src/Actions/loginAction.js
+++ src/Actions/loginAction.js
@@ -2,4 +2,10 @@ import axios from 'axios';
 
 export default(data)=>{
     const loginUrl = `${window.apiHost}/users/login`
+    const axiosResponse = axios.post(loginUrl,data);
+
+    return {
+        type: "login",
+        payload: axiosResponse
+    }
 }

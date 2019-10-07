ea2fd4348017ea50e2eed831fed26a40dbb7983a
Kevin Reid <kevinreid3light@gmail.com>
2019-10-07 09:31:56 -0400

    socket changes

--- src/connection/connection.js
+++ src/connection/connection.js
@@ -1,3 +1,12 @@
 import openSocket from 'socket.io-client';
 
-const socket = openSocket('localhost:7000')
+const socket = openSocket('localhost:3100');
+
+
+function sayHi(){
+    console.log('msg sent')
+    socket.emit('msg', 'hello')
+}
+sayHi();
+
+export { sayHi }

180ea5d096b3cd6b2b215c14a9e0622dd176660b
Jonathan <jonathan@boldideas.net>
2019-10-13 19:00:25 -0400

    adds accept and decline buttons on the user dashboard

--- src/Component/UserDashboard/DisplayUserTickets.js
+++ src/Component/UserDashboard/DisplayUserTickets.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import {subUserTickets,userTicketInfo} from '../../connection/connection'
+import axios from 'axios';
 
 class DisplayUserTickets extends React.Component{
   constructor(props){
@@ -16,13 +17,48 @@ class DisplayUserTickets extends React.Component{
     })})
   }
 
+   fuckingbuttonMrReidhahahahaAccept =async (e) =>{
+    let userValue 
+    
+    switch(e.target.value){
+      case 'accept':
+        userValue = true
+        break;
+      case 'decline':
+        userValue = false
+    }
+    let url =`${window.apiHost}/ticket/confirm-ticket-quote`
+    let axiosResponse = await axios.put(url,{
+      userValue,
+      token: this.props.userInfo.authToken, 
+      userId: this.props.userInfo.id,
+      ticketId: this.state.tickets[0].id,
+      progress: this.state.tickets[0].progress 
 
+    }
+    )
+      
+    console.log(axiosResponse)
+  }
+
+  
 
   render(){
+    console.log('_+_+_+_+_+_+_+_', this.props)
+    let ticketInfo = this.state.tickets[0] === undefined ? 
+    <div> loading </div>
+    : 
+    <>
+      <div>{this.state.tickets[0].name}</div>
+      <div>{this.state.tickets[0].order_items}</div>
+      <button onClick={this.fuckingbuttonMrReidhahahahaAccept} value="accept">Accept</button>
+      <button onClick={this.fuckingbuttonMrReidhahahahaAccept} value="decline">Decline</button>
+    </>
       console.log(this.state.tickets)
       console.log(this.props)
     return(<>
-      <h1> Hello world. </h1>
+      {ticketInfo}
+      {/* <h1> {} </h1> */}
     </>)
   }
 }
